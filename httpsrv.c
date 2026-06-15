20830a4f867a037f69d446857de3649a763811e6
texturawasd <texturawasd@gmail.com>
2026-06-14 23:23:32 -0300

    http server

---
+++ httpsrv.c
@@ -0,0 +1,114 @@
+#ifndef _DO_AS_I_SAY
+#error THIS SERVER IS NOT UP TO STANDARDS, DO NOT USE IT. It is an example server
+#endif
+
+/* simple HTTP server. */
+#ifdef _DO_AS_I_SAY
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdatomic.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#ifndef PORT
+#define PORT        80
+#endif
+#define BUFFER_SIZE 4096
+
+volatile sig_atomic_t running = 1;
+
+void handle_sigint(int sig){
+    (void)sig;      /* unused */
+    running = 0;    /* just set the flag to 0 */
+}
+
+int main(void)
+{
+    struct sigaction sa = {0};
+    sa.sa_handler = handle_sigint;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (server_fd < 0) { perror("socket"); return EXIT_FAILURE; }
+
+    int opt = 1;
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+        perror("bind");
+        return EXIT_FAILURE;
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        perror("listen");
+        return EXIT_FAILURE;
+    }
+
+    #ifndef _ONESHOT_HTTPSRV
+    do {
+    #endif
+        /* server shall serve now */
+        printf("serving on port %d\n", PORT);
+
+        int client = accept(server_fd, NULL, NULL);
+        if (client < 0) continue;
+
+        char request[BUFFER_SIZE];
+        read(client, request, sizeof(request) - 1);
+
+        FILE *f = fopen("index.html", "rb");
+        if (!f) { puts("[!!!!!]: file index.html not found");
+        const char *notfound =
+            "HTTP/1.1 404 not found :(\r\n"
+            "Content-Type: text/plain\r\n"
+            "Content-Length: 13\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "404 Not Found";
+        write(client, notfound, strlen(notfound));
+        close(client);
+        continue; }
+
+        fseek(f, 0, SEEK_END);
+        long len = ftell(f);
+        rewind(f);
+
+        char *body = malloc(len);
+        fread(body, 1, len, f);
+        fclose(f);
+
+        char header[256];
+        int n = snprintf(
+            header, sizeof(header),
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: %ld\r\n"
+            "Connection: close\r\n"
+            "\r\n",
+            len);
+        write(client, header, n);
+        write(client, body, len);
+
+        free(body);
+        close(client);
+    #ifndef _ONESHOT_HTTPSRV
+    } while (running);
+    #endif
+
+    /* cleanup SHOULD happen here*/
+
+    return EXIT_SUCCESS;
+}
+#endif
